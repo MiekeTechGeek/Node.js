c500eaf350e4989c3d04b89bd4fe2c5fce793e8b
Mieke Pienaar <miekepienaar435@gmail.com>
2025-04-24 19:09:34 +0200

    first commit

---
+++ my_code/unit2/lesson10/express_templates/quickCheck.js
@@ -0,0 +1,13 @@
+// Quick check 10.1 What is a templating engine?
+// = A templating engine is the tool that Express.js uses in your application to process
+// a template view. Because template views contain a mix of HTML and JavaScript content, the engine’s job
+// is to convert this information to an HTML file that your browser can use. 
+
+// Quick check 10.2 What is the format in which you send data from your controller to a view?
+// = To send data from your controller, you can pass a variable within a JavaScript
+// object. The variable that’s local to your controller’s context follows the key, whose name should match
+// the variable name in your view.
+
+// Quick check 10.3 What keyword do you use to share partials across multiple views?
+// = The include keyword looks for a partial in the relative path provided and renders
+// it in place. 
