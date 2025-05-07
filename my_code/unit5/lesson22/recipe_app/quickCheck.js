dd278688d866e88255be49d3c2d9b1e83a328b93
Mieke Pienaar <miekepienaar435@gmail.com>
2025-05-07 18:29:05 +0200

    first commit

---
+++ my_code/unit5/lesson22/recipe_app/quickCheck.js
@@ -0,0 +1,7 @@
+// Quick check 22.1 How does a cookie’s secret key change the way that data is sent and stored on a browser?
+// = The secret key used with cookies allows data to have some encryption. Encryption
+// is important for securing the data sent over the internet and for making sure that the data living in the
+// user’s browser isn’t exposed to modifications. 
+
+// Quick check 22.2 What two arguments are needed for the req.flash method?
+// = req.flash needs a flash-message type and a message.
